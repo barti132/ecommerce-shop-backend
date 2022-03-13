70676570ea1434f9afe31a3c08bd490ef905d8ae
barti132 <barteksredzinski@gmail.com>
2022-03-13 01:31:26 +0100

    add jwt to swagger

--- src/main/java/pl/bartoszsredzinski/ecommerceshopv1/config/SwaggerConfig.java
+++ src/main/java/pl/bartoszsredzinski/ecommerceshopv1/config/SwaggerConfig.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 /**
  * Spring fox configuration
@@ -21,23 +23,40 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig{
     @Bean
-    public Docket swaggerConfiguration() {
-        return new Docket(DocumentationType.SWAGGER_2)
+    public Docket swaggerConfiguration(){
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(getApiInfo())
+                .securityContexts(List.of(securityContext()))
+                .securitySchemes(List.of(apiKey()))
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("pl.bartoszsredzinski.ecommerceshopv1.controller"))
                 .paths(PathSelectors.any())
-                .build().
-                apiInfo(getApiInfo());
+                .build();
     }
 
     private ApiInfo getApiInfo(){
-        return new ApiInfoBuilder()
-                .title("Ecommerce shop API")
+        return new ApiInfoBuilder().title("Ecommerce shop API")
                 .version("1.0")
-                .description("API for ecommerce shop application")
+                .description("API for ecommerce shop application.\nExample username and password - user1/user1." +
+                        " To authorize sign in with /auth/login, copy the jwt and past in authorize option with prefix Bearer.")
                 .contact(new Contact("Bartosz Średziński", "http://bartoszsredzinski.pl", "barteksredzinski@gmail.com"))
                 .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
                 .build();
+    }
+
+    private ApiKey apiKey(){
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext(){
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth(){
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return List.of(new SecurityReference("JWT", authorizationScopes));
     }
 
 }
